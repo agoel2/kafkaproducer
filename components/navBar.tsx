b95b359f98a4efba32eae838a76fd62f3e9b05db
Ankit <agoel@tacitknowledge.com>
2023-01-29 15:50:22 +0000

    styling

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export const NavBar = () => {
     return (
         <div><header className="text-gray-600 body-font">
-            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <div className="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center">
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     <Link href="/">Producer</Link>
                     <Link href="/consumer">Consumer</Link>
