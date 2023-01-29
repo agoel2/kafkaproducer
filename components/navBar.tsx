569f00d502b51af3432f4fca1397bfdfbe850741
Ankit <agoel@tacitknowledge.com>
2023-01-29 18:31:20 +0000

    further styling fix

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
+import styles from "../styles/Home.module.css";
 
 export const NavBar = () => {
     return (
-        <div><header className="text-gray-600 body-font">
+        <main className={styles.navbar}><header className="text-gray-600 body-font">
             <div className="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center">
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     <Link href="/">Producer</Link>
@@ -11,6 +12,6 @@ export const NavBar = () => {
                     <Link href="/materialized-views">Materialized Views</Link>
                 </nav>
             </div>
-        </header></div>
+        </header></main>
     )
 }
