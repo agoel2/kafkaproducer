5120b4c776d4a1124548244f5cff5f57ecdb76c1
Ankit <agoel@tacitknowledge.com>
2023-02-18 20:22:38 +0000

    add samples from s3

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -5,6 +5,7 @@ import styles from "../styles/Home.module.css";
 export const NavBar = () => {
     return (
         <main className={styles.navbar}><header className="text-gray-600 body-font">
+
             <div className="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center">
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     <Link href="/">Producer</Link>
@@ -12,6 +13,7 @@ export const NavBar = () => {
                     <Link href="/materialized-views">Materialized Views</Link>
                     <Link href="/generator">Generator</Link>
                     <Link href="/validator">Validator</Link>
+                    <Link href="/samples">Samples</Link>
                 </nav>
             </div>
         </header></main>
