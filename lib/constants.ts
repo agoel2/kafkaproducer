e3881cf831065b4a92d4eff8994bcf6bb3ddccec
Ankit <agoel@tacitknowledge.com>
2023-03-01 16:46:49 +0000

    some refactor

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,4 @@
 export const DEFAULT_ENV = 'dev';
+
+// export const LAMBDA_URL = 'https://4sp6tubgjaiuqfdgfxz5itmnai0xgsdo.lambda-url.eu-west-1.on.aws/';
+export const LAMBDA_URL = 'https://r2ed4uwgleipxlbaw22bkkkksi0jirvd.lambda-url.eu-west-1.on.aws/';
